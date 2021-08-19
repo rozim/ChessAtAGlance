d2cb69ff97f59c830ed9b538054f67f7b1a0cfd2
Dave Spencer <david.spencerian@gmail.com>
2021-08-19 09:30:59 -0700

    show move predictions

--- playout.py
+++ playout.py
@@ -19,6 +19,9 @@ flags.DEFINE_string('plan', None, 'toml file')
 flags.DEFINE_string('model', None, '')
 
 
+def f2(f):
+  return f'{f:.2f}'
+      
 def main(argv):
   flags.mark_flags_as_required(['model'])
   tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
@@ -27,13 +30,24 @@ def main(argv):
   model = tf.keras.models.load_model(FLAGS.model)
   
   game = pyspiel.load_game('chess')
+  #game.deserialize_state('rnbqk2r/5pb1/p1pppnp1/1p5p/3P4/PPPBPNPP/5P2/RNBQK2R w KQkq - 1 10')
 
-  print('game: ', game)
-  state = game.new_initial_state()
+  #help(game)
 
-  legal = state.legal_actions_mask()
+  #print('game: ', game)
+  #print(game.new_initial_state(fen).legal_actions())
+  #print(game.new_initial_state().legal_actions()  )
+  #fen = 'rnbqk2r/5pb1/p1pppnp1/1p5p/3P4/PPPBPNPP/5P2/RNBQK2R w KQkq - 1 10'
+  fen = None
+  if fen:
+    state = game.new_initial_state(fen)
+  else:
+    state = game.new_initial_state()
+  #help(state)
+
   print(state)
-  print(state.is_terminal())
+
+  #sys.exit(0)
 
   sans = []
   while not state.is_terminal():
@@ -46,12 +60,24 @@ def main(argv):
     
     logits2 = []
     i2a = []
+
     for i, a in enumerate(state.legal_actions()):
       logits2.append(logits[a])
       i2a.append(a)
 
     softmax2 = tf.nn.softmax(np.array(logits2, dtype='float32')).numpy()
     softmax2 /= softmax2.sum() # try to get closer to 1.0
+
+    hack = []    
+    for i, score in enumerate(softmax2):
+      hack.append((score,
+                  state.action_to_string(i2a[i])))
+    hack.sort(reverse=True)
+
+    for ent in hack:
+      if ent[0] < 0.02:
+        break
+      print(f2(ent[0]), ent[1])
 
     choice_action = np.random.choice(i2a, p=softmax2)
     san = state.action_to_string(choice_action)
