71228afc7e3da1353f3dad1d381ec76013097f58
Dave Spencer <david.spencerian@gmail.com>
2021-08-19 22:50:57 -0700

    fix eof

--- snappy_io.py
+++ snappy_io.py
@@ -13,15 +13,22 @@ def unsnappy(fn):
     uncompress  = snappy.uncompress    
     read = f.read    
     while True:
-      n = unpack('@i', read(4))[0]
+      blob = read(4)
+      if len(blob) == 0:
+        return
+      n = unpack('@i', blob)[0]
       yield tf.train.Example().FromString(snappy.uncompress(read(n)))
 
 
-
 def main(argv):
+  n = 0
+  mod = 1
   for ex in unsnappy('mega-v2-9.snappy'):
-    print(ex)
-    break
+    n += 1
+    if n % mod == 0:
+      print(n)
+      mod *= 2
+  print('done', n)
 
 if __name__ == '__main__':
   app.run(main)          
