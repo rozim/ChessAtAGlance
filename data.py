2f55ea38df7d0fe0f18fbd4aa8ef395f7bbb9bdd
Dave Spencer <david.spencerian@gmail.com>
2021-08-24 22:18:58 -0700

    multiple files

--- data.py
+++ data.py
@@ -25,22 +25,33 @@ def gen_snappy(fn):
     yield (board, action)
     
   
-def create_input_generator(dplan, fn, is_train=True, verbose=True):
-  assert os.path.isfile(fn), fn
+def create_input_generator(dplan, fns, is_train=True, verbose=True):
+  if type(fns) == type(""):
+    fns = [fns]
   if verbose:
-    print(f'Open {fn}')
-  gen1 = functools.partial(gen_snappy, fn)
-  ds1 = tf.data.Dataset.from_generator(gen1,
-                                      output_types=('float32', 'int64'),
-                                      output_shapes=(BOARD_SHAPE, []))
+    print(f'Open {fns}')
+
+  datasets = []
+  for fn in fns:
+    assert os.path.isfile(fn), fn
+    gen1 = functools.partial(gen_snappy, fn)
+    datasets.append(
+      tf.data.Dataset.from_generator(gen1,
+                                     output_types=('float32', 'int64'),
+                                     output_shapes=(BOARD_SHAPE, [])).repeat())
+    
+  ds = tf.data.experimental.sample_from_datasets(
+    datasets,
+    weights=None # Uniform
+    )
   if is_train:
-    ds1 = ds1.shuffle(dplan.shuffle)
-  ds1 = ds1.repeat()
-  ds1 = ds1.batch(dplan.batch,
-                  num_parallel_calls=AUTOTUNE,
-                  deterministic=False) # performance
-  ds1 = ds1.prefetch(dplan.prefetch)
-  return ds1    
+    ds = ds.shuffle(dplan.shuffle)  
+  ds = ds.repeat()
+  ds = ds.batch(dplan.batch,
+                num_parallel_calls=AUTOTUNE,
+                deterministic=False) # performance
+  ds = ds.prefetch(dplan.prefetch)
+  return ds
 
     
 def main(argv):
