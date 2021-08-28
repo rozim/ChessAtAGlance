287341e5941971b5f22801d88f6a194f39e5d795
Dave Spencer <david.spencerian@gmail.com>
2021-08-28 11:03:56 -0700

    decent impl, url update

--- timing_callback.py
+++ timing_callback.py
@@ -11,65 +11,77 @@ class TimingCallback(Callback):
     super(TimingCallback, self).__init__()
     self.t = {}
     self.tot = collections.defaultdict(float)
+    self.num = collections.defaultdict(int)
 
   def on_train_batch_begin(self, batch, logs=None):
-    self._begin('train_batch')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_train_batch_end(self, batch, logs=None):
-    self._end('train_batch')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_test_batch_begin(self, batch, logs=None):
-    self._begin('test_batch')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_test_batch_end(self, batch, logs=None):
-    self._end('test_batch')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_predict_batch_begin(self, batch, logs=None):
-    self._begin('predict_batch')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_predict_batch_end(self, batch, logs=None):
-    self._end('predict_batch')
+    self._doit(sys._getframe().f_code.co_name)
 
   #####
 
 
   def on_epoch_begin(self, epoch, logs=None):
-    self._begin('epoch')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_train_begin(self, logs=None):
-    self._begin('train')
-    #print('xxx', sys._getframe(  ).f_code.co_name)
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_test_begin(self, logs=None):
-    self._begin('test')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_predict_begin(self, logs=None):
-    self._begin('predict')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_epoch_end(self, epoch, logs=None):
-    self._end('epoch')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_train_end(self, logs=None):
-    self._end('train')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_test_end(self, logs=None):
-    self._end('test')
+    self._doit(sys._getframe().f_code.co_name)
 
   def on_predict_end(self, logs=None):
-    self._end('predict')
+    self._doit(sys._getframe().f_code.co_name)
 
   #
-  def _begin(self, what):
-    self.t[what] = time.perf_counter()
-
-  def _end(self, what):
-    self.tot[what] += (time.perf_counter() - self.t[what])
+  # maybe more generic again is
+  # https://stackoverflow.com/questions/2704434/intercept-method-calls-in-python
+  def _doit(self, func_name):
+    if func_name.endswith('_begin'):
+      what = func_name.removesuffix('_begin')
+      self.t[what] = time.perf_counter()
+    else:
+      assert func_name.endswith('_end')
+      what = func_name.removesuffix('_end')
+      assert self.t[what]
+      self.tot[what] += (time.perf_counter() - self.t[what])
+      self.num[what] += 1
+      self.t[what] = None
 
 
 
 def main(argv):
   cb = TimingCallback()
   cb.on_train_begin()
+  cb.on_train_batch_begin(None)
+  cb.on_train_batch_end(None)
+  cb.on_train_batch_begin(None)
+  cb.on_train_batch_end(None)
   cb.on_train_end()
   print(cb.tot)
 
