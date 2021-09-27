eee6f22a79bfa3966659e6af31b4f795250aff82
Dave Spencer <david.spencerian@gmail.com>
2021-09-27 13:06:44 -0700

    be much more like the paper

--- easy-to-hard-redux/generate_training_data.py
+++ easy-to-hard-redux/generate_training_data.py
@@ -1,15 +1,14 @@
 import warnings
 import sys, os
-from open_spiel.python.observation import make_observation
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-import pyspiel
 from absl import app
 from absl import flags
 from absl import logging
 import time
+import chess
 
 FEATURES = {
   'board': tf.io.FixedLenFeature((1280,), tf.float32),
@@ -17,6 +16,48 @@ FEATURES = {
   'rating': tf.io.FixedLenFeature([], tf.int64),
   'uci': tf.io.FixedLenFeature([], tf.string),
 }
+
+# from easy-to-hard-data/make_chess.py
+def get_board_tensor(board_str, black_moves):
+  """ function to move from FEN representation to 12x8x8 tensor
+    Note: The rows and cols in the tensor correspond to ranks and files
+          in the same order, i.e. first row is Rank 1, first col is File A.
+          Also, the color to move next occupies the first 6 channels."""
+
+  p_to_int = {"k": 1, "q": 2, "b": 3, "n": 4, "r": 5, "p": 6,
+                "K": 7, "Q": 8, "B": 9, "N": 10, "R": 11, "P": 12}
+  new_board = np.zeros((8, 8))
+  rank = 7
+  file = 0
+  for p in board_str:
+    if p == "/":
+      rank -= 1
+      file = 0
+    elif not p.isdigit():
+      new_board[rank, file] = (p_to_int[p])
+      file += 1
+    else:
+      new_board[rank, file:file+int(p)] = 0
+      file += int(p)
+
+  board_tensor = np.zeros((12, 8, 8))
+  pieces = "kqbnrpKQBNRP" if black_moves else "KQBNRPkqbnrp"
+  for p_i, p in enumerate(pieces):
+    board_tensor[p_i] = (new_board == p_to_int[p])
+
+  return board_tensor
+
+def get_moves_tensor(move):
+  file_to_num = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+  origin_file = file_to_num[move[0]]
+  origin_rank = int(move[1]) - 1
+  dest_file = file_to_num[move[2]]
+  dest_rank = int(move[3]) - 1
+  move = np.zeros((8, 8))
+  move[origin_rank, origin_file] = 1
+  move[dest_rank, dest_file] = 1
+  return move
+
 
 def _float_feature(value):
   """Returns a float_list from a float / double."""
@@ -42,13 +83,30 @@ def _extract(blob):
           t['best_move'])
 
 
+
+def sq2i(sq):
+  # e.g. h2
+  row = ord(sq[0]) - ord('a')
+  col = ord(sq[1]) - ord('1')
+  return row * 8 + col
+
+def move2i(m):
+  return (sq2i(m[0:2]),
+          sq2i(m[2:4]))
+
+
 def main(argv):
+  # print(sq2i('a1'))
+  # print(sq2i('h1'))
+  # print(sq2i('h8'))
+  # print(move2i('a1h1'))
+
   tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
   logging.set_verbosity('error')
   os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
   warnings.filterwarnings('ignore', category=Warning)
 
-  if True:
+  if False:
     ds = tf.data.TFRecordDataset(['easy.rio'], 'ZLIB', num_parallel_reads=1)
     ds = ds.map(_extract)
     ds = ds.batch(2)
@@ -60,8 +118,6 @@ def main(argv):
       break
     sys.exit(0)
 
-  game = pyspiel.load_game('chess')
-
   df = pd.read_csv('lichess_db_puzzle.csv')
   t0 = time.time()
   mod = 1024
@@ -69,30 +125,34 @@ def main(argv):
     compression_type='ZLIB',
     output_buffer_size=(4 * 1024 * 1024))
 
-  #rio.write(ex.SerializeToString())
-  #df = df.head(100000)
   df = df.sort_values('Rating')
-  with tf.io.TFRecordWriter('easy.rio', opts) as rio:
+  with tf.io.TFRecordWriter('easy-v3.rio', opts) as rio:
 
     index = -1
     for db_index, row in df.iterrows():
       index += 1
       rating = row['Rating']
-      best_move = row['Moves'].split(' ')[0] # first
-      fen = row['FEN']
-      state = game.new_initial_state(fen)
 
-      if state.current_player() < 0:
-        continue
-      action = state.parse_move_to_action(best_move) # int
-      board = state.observation_tensor()
+      mistake = chess.Move.from_uci(row['Moves'].split(' ')[0])
+      refutation = row['Moves'].split(' ')[1]
+      board = chess.Board(row['FEN'])
+      board.push(mistake)
+      new_fen = board.fen()
+
+      black_moves = {'w': 0, 'b': 1}[new_fen.split(' ')[1]]
+      board_str = new_fen.split(' ')[0]
+      puzzle_tensor = get_board_tensor(board_str, black_moves)
+      moves_tensor = get_moves_tensor(refutation) # The refutation
 
       feature = {
-        'board': tf.train.Feature(float_list=tf.train.FloatList(value=board)),
-        'uci':  _bytes_feature(best_move.encode('utf-8')),
+        'puzzle': tf.train.Feature(float_list=tf.train.FloatList(value=puzzle_tensor.flatten())),
+        'solution': tf.train.Feature(float_list=tf.train.FloatList(value=moves_tensor.flatten())),
+
+        'refutation_uci':  _bytes_feature(refutation.encode('utf-8')),
+        'new_fen':  _bytes_feature(new_fen.encode('utf-8')),
         'rating':  _int64_feature(rating),
-        'best_move': _int64_feature(action),
       }
+
       pb = tf.train.Example(features=tf.train.Features(feature=feature))
       rio.write(pb.SerializeToString())
 
