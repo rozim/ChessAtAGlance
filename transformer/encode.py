15a2090cb72d6f1d624d9dfe6d625b50472a318d
Dave Spencer <david.spencerian@gmail.com>
2022-11-07 15:36:09 -0800

    prelim transformer encoding

--- transformer/encode.py
+++ transformer/encode.py
@@ -7,9 +7,11 @@ from chess import PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
 from absl import app
 from absl import flags
 
-SHAPE = (12, 64)
-SHAPE_2D = (12, 8, 8)
-FLAT_SHAPE = 12 * 64
+CNN_SHAPE = (12, 64)
+CNN_SHAPE_2D = (12, 8, 8)
+CNN_FLAT_SHAPE = 12 * 64
+
+TRANSFORMER_SHAPE = (64 + 1 + 4)
 
 # P2I {
 #   PAWN: 0,
@@ -20,14 +22,14 @@ FLAT_SHAPE = 12 * 64
 #   KING: 5}
 
 CO_P2I = [
-  {
+  { # black
     PAWN: 0,
     KNIGHT: 1,
     BISHOP: 2,
     ROOK: 3,
     QUEEN: 4,
     KING: 5},
-  {
+  { # white
     PAWN: 6,
     KNIGHT: 7,
     BISHOP: 8,
@@ -35,6 +37,24 @@ CO_P2I = [
     QUEEN: 10,
     KING: 11}
 ]
+
+TRANSFORMER_CO_P2I = [
+  { # black
+    PAWN: 1,
+    KNIGHT: 2,
+    BISHOP: 3,
+    ROOK: 4,
+    QUEEN: 5,
+    KING: 6},
+  { # white
+    PAWN: 7,
+    KNIGHT: 8,
+    BISHOP: 9,
+    ROOK: 10,
+    QUEEN: 11,
+    KING: 12}
+]
+TRANSFORMER_VOCABULARY = (1 + 12)
 
 # I2P = {
 #   0: PAWN,
@@ -62,8 +82,8 @@ CO_I2P = {
 }
 
 
-def encode_board(board):
-  ar = np.zeros(SHAPE)
+def encode_cnn_board(board):
+  ar = np.zeros(CNN_SHAPE)
   for piece in [PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING]:
     for color in [WHITE, BLACK]:
       for sq in board.pieces(piece, color):
@@ -71,6 +91,22 @@ def encode_board(board):
         #print(piece, color, sq, index)
         assert ar[index][sq] == 0.0
         ar[index][sq] = 1.0
+  return ar
+
+def encode_transformer_board(board):
+  ar = np.zeros(TRANSFORMER_SHAPE)
+  for piece in [PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING]:
+    for color in [WHITE, BLACK]:
+      for sq in board.pieces(piece, color):
+        assert ar[sq] == 0.0
+        print('set', sq, TRANSFORMER_CO_P2I[color][piece])
+        ar[sq] = TRANSFORMER_CO_P2I[color][piece]
+
+  ar[64] = 1.0 if board.turn else 0.0
+  ar[65] = 1.0 if board.has_kingside_castling_rights(WHITE) else 0.0
+  ar[66] = 1.0 if board.has_queenside_castling_rights(WHITE) else 0.0
+  ar[67] = 1.0 if board.has_kingside_castling_rights(BLACK) else 0.0
+  ar[68] = 1.0 if board.has_queenside_castling_rights(BLACK) else 0.0
   return ar
 
 
@@ -83,7 +119,9 @@ def main(argv):
     for co in [WHITE, BLACK]:
       print('p=', p, 'co=', co, list(board.pieces(p, co)))
   print()
-  print(encode(board))
+  print(encode_cnn_board(board))
+  print()
+  print(encode_transformer_board(board))
   print()
   print(board)
 
