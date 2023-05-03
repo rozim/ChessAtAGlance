95593a8765e0800eb1722d1e670222c5bce92f07
Dave Spencer <david.spencerian@gmail.com>
2023-05-02 23:32:32 -0700

    smelu prelim

---
+++ smelu.py
@@ -0,0 +1,17 @@
+
+import tensorflow as tf
+
+# Smooth activations and reproducibility in deep networks
+# https://arxiv.org/abs/2010.09931
+
+from keras.layers import Activation
+from keras.utils.generic_utils import get_custom_objects
+
+
+def smelu(x, beta=1.0):
+  smooth = (0.25 / beta) * x**2 + 0.5 * x * beta / 4
+  y = tf.zeros_like(x)
+  y = tf.where((x > -beta) & (x < beta), smooth, y)
+  return tf.where(x > beta, x, y)
+
+get_custom_objects().update({'smelu': Activation(smelu)})
