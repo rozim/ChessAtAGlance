1d3106fa0dabd03a99cd7303cd3fab072bae424a
Dave Spencer <david.spencerian@gmail.com>
2023-05-07 10:37:05 -0700

    use PiecewiseConstantDecay

--- lr.py
+++ lr.py
@@ -1,13 +1,19 @@
+import functools
+import sys
+
 from absl import app
 from plan import *
-import functools
+
+
+import tensorflow as tf
 from tensorflow.keras.optimizers.schedules import CosineDecayRestarts
 from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
 
-def create_lr_schedule(tplan):
+def create_lr_scheduler(tplan):
   """ Returns callback"""
   if tplan.lr_schedule == 'warm_linear':
-    return (create_warm_linear_schedule(tplan), tplan.lr)
+    return (create_warm_linear_scheduler(tplan), tplan.lr)
   assert tplan.lr_schedule == 'cosine'
 
   lr = CosineDecayRestarts(initial_learning_rate=tplan.lr,
@@ -18,8 +24,9 @@ def create_lr_schedule(tplan):
   return lr
 
 
-def create_warm_linear_schedule(tplan):
-  def _create_warm_linear_schedule(epoch, ignore_lr):
+
+def create_warm_linear_scheduler(tplan):
+  def _create_warm_linear_scheduler(epoch, ignore_lr):
     lr, train_epochs, train_warmup = (tplan.lr,
                                       tplan.epochs,
                                       tplan.warmup)
@@ -32,13 +39,37 @@ def create_warm_linear_schedule(tplan):
       pct = (epoch - train_warmup) / (train_epochs - train_warmup)
       pct *= tplan.get('lr_max_decay_factor', 1.0)
       return lr - (pct * lr)
-  return LearningRateScheduler(_create_warm_linear_schedule)
+  return LearningRateScheduler(_create_warm_linear_scheduler)
+
+
+def create_warm(tplan):
+  epochs = tplan.epochs
+  warmup = tplan.warmup
+  max_decay_factor = tplan.get('lr_max_decay_factor', 1.0)
+  lr = tplan.lr
+  values = []
+
+  for step in range(epochs):
+    step = step + 1
+    if step <= warmup:
+      values.append(lr * (step / warmup))
+    else:
+      pct = (step - warmup) / (epochs - warmup)
+      pct *= max_decay_factor
+      values.append(lr - (pct * lr))
+  boundaries = list(range(0, epochs - 1))
+  return PiecewiseConstantDecay(values=values,
+                                boundaries=boundaries)
 
 
 def main(argv):
   plan = load_plan('config/cnn_tune_1.toml')
   tplan = plan.train
-  lrs = create_warm_linear_schedule(tplan)
+  lr = create_warm(tplan)
+  for e in range(tplan.epochs):
+    print(e, lr(e))
+  sys.exit(0)
+  lrs = create_warm_linear_scheduler(tplan)
   func = lrs.schedule
   for e in range(tplan.epochs):
     print(e, func(e, None))
