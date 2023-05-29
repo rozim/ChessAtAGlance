59d6112c0fb4b24ab75f057533750a967f7dc17f
Dave Spencer <david.spencerian@gmail.com>
2023-05-29 12:24:13 -0700

    bazel, tests, and transformer encode/decode

---
+++ BUILD
@@ -0,0 +1,10 @@
+py_test(
+    name = "encode_test",
+    srcs = ["encode_test.py"],
+    deps = [":encode"]
+)
+
+py_library(
+  name = "encode",
+  srcs = ["encode.py"]
+  )
--- encode.py
+++ encode.py
@@ -7,10 +7,12 @@
 #
 #
 #
+import sys
+
 import numpy as np
 
 import chess
-from chess import WHITE, BLACK
+from chess import WHITE, BLACK, Piece
 from chess import PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
 
 from absl import app
@@ -28,8 +30,12 @@ CNN_SHAPE_3D = (CNN_PLANES, 8, 8)
 CNN_FLAT_SHAPE = CNN_PLANES * 64
 CNN_ONES_PLANE = np.ones(64)
 
-# TRANSFORMER_SIZE = (64 + 1 + 4)
-# TRANSFORMER_VOCABULARY = (1 + 12)
+
+# 64: squares
+# 4: castle
+# 1: ep square
+TRANSFORMER_SIZE = (64 + 4 + 1)
+
 
 CO_P2I = [
   { # black
@@ -50,6 +56,9 @@ CO_P2I = [
 CASTLE_WQ, CASTLE_BQ, CASTLE_WK, CASTLE_BK = (12, 13, 14, 15)
 assert CASTLE_BK == (CNN_PLANES - 1), (CASTLE_BK, CNN_PLANES)
 
+# Unique tokens.
+# reserve 0 for empty square
+# 1..12 for pieces
 TRANSFORMER_CO_P2I = [
   { # black
     PAWN: 1,
@@ -67,22 +76,70 @@ TRANSFORMER_CO_P2I = [
     KING: 12}
 ]
 
+TRANSFORMER_CO_I2P = {
+  1: (BLACK, PAWN),
+  2: (BLACK, KNIGHT),
+  3: (BLACK, BISHOP),
+  4: (BLACK, ROOK),
+  5: (BLACK, QUEEN),
+  6: (BLACK, KING),
 
-CO_I2P = {
-  0: (WHITE, PAWN),
-  1: (WHITE, KNIGHT),
-  2: (WHITE, BISHOP),
-  3: (WHITE, ROOK),
-  4: (WHITE, QUEEN),
-  5: (WHITE, KING),
-
-  6: (BLACK, PAWN),
-  7: (BLACK, KNIGHT),
-  8: (BLACK, BISHOP),
-  9: (BLACK, ROOK),
-  10: (BLACK, QUEEN),
-  11: (BLACK, KING),
+  7: (WHITE, PAWN),
+  8: (WHITE, KNIGHT),
+  9: (WHITE, BISHOP),
+  10: (WHITE, ROOK),
+  11: (WHITE, QUEEN),
+  12: (WHITE, KING),
 }
+
+
+
+TRANSFORMER_INDEX_WK = 64
+TRANSFORMER_INDEX_WQ = 65
+TRANSFORMER_INDEX_BK = 66
+TRANSFORMER_INDEX_BQ = 67
+TRANSFORMER_INDEX_EP = 68
+
+# 13..20 for Castling
+TRANSFORMER_CASTLE_WK = 13
+TRANSFORMER_CASTLE_NO_WK = 14
+TRANSFORMER_CASTLE_WQ = 15
+TRANSFORMER_CASTLE_NO_WQ = 16
+TRANSFORMER_CASTLE_BK = 17
+TRANSFORMER_CASTLE_NO_BK = 18
+TRANSFORMER_CASTLE_BQ = 19
+TRANSFORMER_CASTLE_NO_BQ = 20
+
+# 21..37 for EP squares
+TRANSFORMER_EP = {
+  chess.A3: 21,
+  chess.B3: 22,
+  chess.C3: 23,
+  chess.D3: 24,
+  chess.E3: 25,
+  chess.F3: 26,
+  chess.G3: 27,
+  chess.H3: 28,
+
+  chess.A6: 29,
+  chess.B6: 30,
+  chess.C6: 31,
+  chess.D6: 32,
+  chess.E6: 33,
+  chess.F6: 34,
+  chess.G6: 35,
+  chess.H6: 36,
+
+  None: 37
+
+  }
+
+TRANSFORMER_EP_DECODE = {
+  v: k for k, v in TRANSFORMER_EP.items()
+  }
+
+TRANSFORMER_VOCABULARY = 38
+
 
 
 CNN_FEATURES = {
@@ -132,29 +189,76 @@ def encode_cnn_board_wtm(board):
 
   return ar
 
-def encode_transformer_board(board):
-  assert False, "needs to be revisited circa 2023-04-29"
-  ar = np.zeros(TRANSFORMER_SIZE)
+def encode_transformer_board_move_wtm(board, move):
+  if board.turn == BLACK:
+    # As move was already played.. (confusing)
+    move = chess.Move(chess.square_mirror(move.from_square),
+                      chess.square_mirror(move.to_square),
+                      move.promotion)
+  return (encode_transformer_board_wtm(board),
+          MOVE_TO_INDEX[move.uci()])
+
+def encode_transformer_board_wtm(board):
+  ar = np.zeros(TRANSFORMER_SIZE, dtype=np.int8)
   for piece in [PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING]:
     for color in [WHITE, BLACK]:
       for sq in board.pieces(piece, color):
         assert ar[sq] == 0.0
         # print('set', sq, TRANSFORMER_CO_P2I[color][piece])
         ar[sq] = TRANSFORMER_CO_P2I[color][piece]
+        #print('SET ', 'sq', sq, 'enc', ar[sq], 'p', piece, 'co', color)
 
-  ar[64] = 1.0 if board.turn else 0.0
-  ar[65] = 1.0 if board.has_kingside_castling_rights(WHITE) else 0.0
-  ar[66] = 1.0 if board.has_queenside_castling_rights(WHITE) else 0.0
-  ar[67] = 1.0 if board.has_kingside_castling_rights(BLACK) else 0.0
-  ar[68] = 1.0 if board.has_queenside_castling_rights(BLACK) else 0.0
+  ar[TRANSFORMER_INDEX_WK] = TRANSFORMER_CASTLE_WK if board.has_kingside_castling_rights(WHITE)  else TRANSFORMER_CASTLE_NO_WK
+  ar[TRANSFORMER_INDEX_WQ] = TRANSFORMER_CASTLE_WQ if board.has_queenside_castling_rights(WHITE) else TRANSFORMER_CASTLE_NO_WQ
+  ar[TRANSFORMER_INDEX_BK] = TRANSFORMER_CASTLE_BK if board.has_kingside_castling_rights(BLACK)  else TRANSFORMER_CASTLE_NO_BK
+  ar[TRANSFORMER_INDEX_BQ] = TRANSFORMER_CASTLE_BQ if board.has_queenside_castling_rights(BLACK) else TRANSFORMER_CASTLE_NO_BQ
+  ar[TRANSFORMER_INDEX_EP] = TRANSFORMER_EP[board.ep_square]
+
   return ar
+
+def decode_transformer_board_wtm(ar):
+  board = chess.Board()
+  board.clear_board()
+  for sq in range(64):
+    encoded = ar[sq]
+    if encoded != 0:
+      co, p = TRANSFORMER_CO_I2P[encoded]
+      board.set_piece_at(sq, Piece(p, co))
+      #print('RESTORE ', 'sq', sq, 'enc', encoded, 'p', p, 'co', co, ' ', board.fen())
+
+  castling_fen = ""
+  if ar[TRANSFORMER_INDEX_WK] == TRANSFORMER_CASTLE_WK:
+    castling_fen += "K"
+  if ar[TRANSFORMER_INDEX_WQ] == TRANSFORMER_CASTLE_WQ:
+    castling_fen += "Q"
+
+  if ar[TRANSFORMER_INDEX_BK] == TRANSFORMER_CASTLE_BK:
+    castling_fen += "k"
+  if ar[TRANSFORMER_INDEX_BQ] == TRANSFORMER_CASTLE_BQ:
+    castling_fen += "q"
+
+  board.set_castling_fen(castling_fen)
+
+  board.ep_square = TRANSFORMER_EP_DECODE[ar[TRANSFORMER_INDEX_EP]]
+
+  return board
+
 
 
 
 def main(argv):
   fen = 'r1bq1rk1/4bppp/p2p1n2/npp1p3/4P3/2P2N1P/PPBP1PP1/RNBQR1K1 w - -'
   board = chess.Board(fen)
+  board = chess.Board()
+  print('BEFORE: ', board.fen())
   #board = chess.Board()
+  with np.printoptions(linewidth=999):
+    encoded = encode_transformer_board_wtm(board)
+    print(encoded)
+  board = decode_transformer_board_wtm(encoded)
+  print(board)
+  print('AFTER: ', board.fen())
+  sys.exit(0)
 
   for p in [PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING]:
     for co in [WHITE, BLACK]:
@@ -167,7 +271,6 @@ def main(argv):
     print(i, np.nonzero(encoded[i]))
   for i in range(12, 16):
     print('castle', i, np.all(encoded[i] == CNN_ONES_PLANE))
-  #print(encode_transformer_board(board))
 
   print()
   print(board)
