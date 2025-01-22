07158a53fdeaceecb245d1ca291333ca0bcf9362
Dave Spencer <david.spencerian@gmail.com>
2025-01-22 13:19:05 -0800

    Show all decent moves within a delta

--- 2025/generate_training_data_with_sf.py
+++ 2025/generate_training_data_with_sf.py
@@ -30,6 +30,8 @@ flags.DEFINE_string('pgn', 't1.pgn', 'PGN file or pattern')
 flags.DEFINE_string('out', '', 'Output, jsonlines')
 flags.DEFINE_string('engine', './stockfish', '')
 flags.DEFINE_integer('search_depth', 6, '')
+flags.DEFINE_integer('multipv', 0, '')
+flags.DEFINE_integer('delta', 0, 'For when using multipv')
 
 HASH = 1024
 THREADS = 1
@@ -83,22 +85,66 @@ def munch(pgn: str, fp, already: set, engine):
             board.pop()
             continue
           already.add(sfen2)
-          res3 = engine.analyse(board, chess.engine.Limit(depth=FLAGS.search_depth))
-          move3 = res3['pv'][0]
-          san3 = board.san(move3)
-          enc_board3, py_move3 = encode_cnn_board_move_wtm(board, move3)
-          py_board3 = enc_board3.astype(int).flatten().tolist()
 
-          # hack = chess.Board(sfen2)
-          # assert hack.is_legal(move3), [hack.fen(), move3.uci()]
-          # engine.quit()
-          # sys.exit(0)
-          j = {'sfen': sfen2,
-               'board_1024': py_board3,
-               'move_1968': py_move3,
-               'move_uci': move3.uci(),
-               'move_san': san3}
-          writer.write(j)
+          if FLAGS.multipv == 1:
+            res3 = engine.analyse(board, chess.engine.Limit(depth=FLAGS.search_depth))
+            move3 = res3['pv'][0]
+            san3 = board.san(move3)
+            enc_board3, py_move3 = encode_cnn_board_move_wtm(board, move3)
+            py_board3 = enc_board3.astype(int).flatten().tolist()
+
+            # hack = chess.Board(sfen2)
+            # assert hack.is_legal(move3), [hack.fen(), move3.uci()]
+            # engine.quit()
+            # sys.exit(0)
+            j = {'sfen': sfen2,
+                 'board_1024': py_board3,
+                 'move_1968': py_move3,
+                 'move_uci': move3.uci(),
+                 'move_san': san3}
+            writer.write(j)
+          else:
+            multi3 = engine.analyse(board, chess.engine.Limit(depth=FLAGS.search_depth), multipv=FLAGS.multipv)
+            first = multi3[0]['score'].white()
+            first_mate = first.is_mate()
+            # if first.score() is None:
+            #   print(board.fen())
+            #   print(multi3)
+            #   sys.exit(0)
+            #   continue
+            for i3, res3 in enumerate(multi3):
+              white = res3['score'].white()
+              try:
+                if first_mate:
+                  if not white.is_mate():
+                    continue
+                elif white.is_mate():
+                  continue # confusing scenario?!
+                elif abs(white.score() - first.score()) > FLAGS.delta:
+                  continue
+              except TypeError:
+                import pprint
+                print(res3['score'].white().is_mate())
+                print(res3['score'].black().is_mate())
+                print(res3['score'].white().mate())
+                print(res3['score'].black().mate())
+                print(first.mate())
+                pprint.pprint(multi3)
+                engine.quit()
+                sys.exit(123)
+              move3 = res3['pv'][0]
+
+              san3 = board.san(move3)
+              enc_board3, py_move3 = encode_cnn_board_move_wtm(board, move3)
+              py_board3 = enc_board3.astype(int).flatten().tolist()
+              j = {'sfen': sfen2,
+                   'board_1024': py_board3,
+                   'move_1968': py_move3,
+                   'move_uci': move3.uci(),
+                   'move_san': san3,
+                   'multi': i3}
+              writer.write(j)
+
           board.pop()
 
   return ng, nm
